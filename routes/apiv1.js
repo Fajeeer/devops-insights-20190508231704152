566b8e03435d469b6ee7d10a580e9695ffdb8fa9
Fajer Alblooshy <fajer.alboushi@gmail.com>
2019-05-13 01:46:31 +0000

    Changes have been made and the original code lines were commented

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -7,15 +7,18 @@ var request = REQUEST.defaults( {
     strictSSL: false
 });
 
-var OPENWEATHERURL = "http://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=imperial";
-
+//var OPENWEATHERURL = "http://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=imperial";
+var OPENWEATHERURL = "http://api.openweathermap.org/data/2.5/weather?q=metric";
 exports.getWeather = function(req, res) {
-	var zip = req.query.zip;
-	if( (zip === null) || (typeof(zip) === 'undefined') ) {
-		return res.status(400).send('zip missing');
+	//var zip = req.query.zip;
+	var cityName = req.query.cityName;
+	//if( (zip === null) || (typeof(zip) === 'undefined') ) {
+	if( (cityName === null) || (typeof cityName === 'undefined') ) {
+		return res.status(400).send('city name missing');
 	}
 
-	var aurl = OPENWEATHERURL + '&zip=' + zip + ',us';
+	//Where New Zealand is defined??
+	var aurl = OPENWEATHERURL + '&cityName=' + cityName + ',nz';
 
 	request({
 		method: 'GET',
@@ -26,8 +29,10 @@ exports.getWeather = function(req, res) {
     		res.status(400).send('Failed to get the data');
     		//console.error("Failed to send request to openweathermap.org", err);
     	} else {
+    		//I think here is where they are using ZIP code
     		if(body.cod === 200) {
-    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' F';
+    			//change from F to C
+    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' C' /*' F'*/;
     			var response = {city: body.name, weather: weath};
     			return res.status(200).send(response);
     		} else {
@@ -41,12 +46,12 @@ router.get('/getWeather', exports.getWeather);
 
 
 exports.getWeather2 = function(req, res) {
-	var zip = req.query.zip;
-	if( (zip === null) || (typeof(zip) === 'undefined') ) {
-		return res.status(400).send('zip missing');
+	var cityName = req.query.cityName;
+	if( (cityName === null) || (typeof cityName === 'undefined') ) {
+		return res.status(400).send('city name missing');
 	}
 
-	var aurl = OPENWEATHERURL + '&zip=' + zip + ',us';
+	var aurl = OPENWEATHERURL + '&cityName=' + cityName + ',nz';
 
 	request({
 		method: 'GET',
@@ -58,7 +63,7 @@ exports.getWeather2 = function(req, res) {
     		//console.error("Failed to send request to openweathermap.org", err);
     	} else {
     		if(body.cod === 200) {
-    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' F';
+    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' C'/*' F'*/;
     			var response = {city: body.name, weather: weath};
     			return res.status(200).send(response);
     		} else {
