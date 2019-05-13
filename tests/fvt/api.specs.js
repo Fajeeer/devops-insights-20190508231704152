76a379bd9c4e925e558227bd884add1037367cef
Fajer Alblooshy <fajer.alboushi@gmail.com>
2019-05-13 02:00:53 +0000

    unfinshed work

--- tests/fvt/api.specs.js
+++ tests/fvt/api.specs.js
@@ -15,14 +15,15 @@
 
     describe('Get Weather', function() {
 
-    	it('with valid zip code', function(done) {
+    	it('with valid city name', function(done) {
         if(!appUrl) {
             assert.fail("Environment variable APP_URL is not defined");
             return done();
         }
         request({
       		method: 'GET',
-              url: appUrl + '/api/v1/getWeather?zip=78613'
+      		//url:appUrl + '/api/v1/getWeather?zip=78613'
+              url: appUrl + '/api/v1/getWeather?cityName=hamilton'
           }, function(err, resp, body) {
           	if(err) {
           		assert.fail('Failed to get the response');
